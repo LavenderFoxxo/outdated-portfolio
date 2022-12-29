575b0dd7524dddde7617f1e8c8eb179af914987c
Alexander Hyman <alexander.hyman05@gmail.com>
2022-12-29 05:10:03 -0800

    Update Port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const port = 3000
+const port = 27504
 
 /* Importing Routes */
 const Main = require('./routes/panel')
