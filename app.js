a92b5887dc3f62d894b6143acae2f617fcb83f67
Alexander Hyman <alexander.hyman05@gmail.com>
2022-12-29 05:14:54 -0800

    BRO

--- app.js
+++ app.js
@@ -14,6 +14,6 @@ app.use('/static', express.static(path.join(__dirname, 'static')))
 app.use('/', Main)
 
 /* Starting the app */
-app.listen(${port}, () => {
+app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`)
 })
